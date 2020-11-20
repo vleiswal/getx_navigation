2c0097a2ab0237bba6636527b9db4215ad7dd6dc
Hugh-Innes Walker <HWalker@bridgestone.co.za>
2020-11-20 13:59:44 +0200

    master

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Text(
+              'This is the HOME screen',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(
+            child: Text('Next Screen'),
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+          RaisedButton(
+            child: Text('Back to Main'),
+            onPressed: () {
+              Get.back();
+            },
+            color: Colors.blueAccent,
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_route_navigation/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Navigation',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('getx Navigation'),
+          ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text('Goto Home'),
+                onPressed: () {
+                  Get.to(
+                    Home(),
+                    fullscreenDialog: true,
+                    transition: Transition.leftToRightWithFade,
+                    duration: Duration(milliseconds: 500),
+                  );
+                },
+              ),
+            ],
+          ))),
+    );
+  }
+}
